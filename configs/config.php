8593b2416f0e57f133a556c3cdcbc8aa10c8379d
namerows <work.kubo@gmail.com>
2015-09-11 23:42:14 +0900

    second commit

---
+++ configs/config.php
@@ -0,0 +1,7 @@
+<?php
+define('INSTALL_DIR', 'sample');
+define('COMMON_LIB_DIR', 'sample');
+define('ZF_DIR', COMMON_LIB_DIR . 'ZendFramework/library/'); 
+define('SMARTY_DIR', COMMON_LIB_DIR . 'Smarty/libs/');
+define('LIB_DIR', INSTALL_DIR . 'application/lib/');
+define('ZF_CONFIG_NAME', 'config'); 
